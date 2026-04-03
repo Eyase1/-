c5897c9ee14faa65c36f6755f903939e53d34bf7
Eyase1 <eyase@qq.com>
2026-04-03 12:28:49 +0800

    Delete common directory

--- common/src/main/java/com/ttt/util/url/BaseUrl.java
+++
@@ -1,7 +0,0 @@
-package com.ttt.util.url;
-
-public class  BaseUrl {
-    public static String baseUrl="https://209973bb.r7.cpolar.cn/";//将本地8080映射到co
-    public static String imageUrl="http://localhost:8080/image/";//加载本地8080图片
-    public static String databaseUrl="5.tcp.vip.cpolar.cn:10683";//将数据库url映射到co
-}
